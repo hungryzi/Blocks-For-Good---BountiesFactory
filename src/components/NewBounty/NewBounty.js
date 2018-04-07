c0252cabdbef3c202ba1f5a91fca7c438872ba14
Zi <hungryzi@gmail.com>
2018-04-07 14:29:09 -0400

    Setup

--- src/components/NewBounty/NewBounty.js
+++ src/components/NewBounty/NewBounty.js
@@ -59,7 +59,7 @@ class NewBounty extends Component {
       loadingAmount: 10,
       loadingString: "",
       containsCode: false,
-      StandardBounties : web3.eth.contract(json.interfaces.StandardBounties).at(json.mainNet.standardBountiesAddress.v1),
+      StandardBounties : web3.eth.contract(json.interfaces.StandardBounties).at(json.rinkeby.standardBountiesAddress.v1),
       UserComments : web3.eth.contract(json.interfaces.UserComments).at(json.mainNet.userCommentsAddress),
       baseURL: json.url.mainNet
     }
